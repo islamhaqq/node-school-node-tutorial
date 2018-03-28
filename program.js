5ff136d775a0ad028eb1f05952557239838f8109
Syed Aman <sna274@nyu.edu>
2018-03-28 11:18:30 +0000

    log the sum of cli arguments

--- program.js
+++ program.js
@@ -1,1 +1,6 @@
-console.log('HELLO WORLD');
+const cliArguments = process.argv.slice(2);
+const sumOfArguments = cliArguments.reduce(
+	(sum, argument) => (sum += +argument),
+	0,
+);
+console.log(sumOfArguments);
